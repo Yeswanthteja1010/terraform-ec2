af24ddf11cfa76367c165cbd29c136ddd41fac4d
Yeswanthteja1010 <111886327+Yeswanthteja1010@users.noreply.github.com>
2025-05-05 12:38:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+    region = var.my_region
+}
 
+module "ec2_module" {
+    source = "./modules/ec2_mod"
+    my_ami = var.p1_ami
+    my_instance_type = var.p1_insta_tyoe
+}
+
+module "s3_module" {
+    source = "./modules/s3_mod"
+    my_buc_name = var.p1_buc_name
+}
